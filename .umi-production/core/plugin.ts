8f2584068b4ff6f864b4b91645580b7d348f0375
b123b123 <02684200@163.com>
2023-07-02 21:22:51 +0800

    优化

---
+++ .umi-production/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from 'C:/Users/wb026/Desktop/javascript-guidebook/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','ssr',],
+});
+
+export { plugin };
