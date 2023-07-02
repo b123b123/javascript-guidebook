d8459c24906b369d5e50492fccc579e784c78f88
b123b123 <02684200@163.com>
2023-07-02 18:54:14 +0800

    图标配置

--- config/config.js
+++ config/config.js
@@ -1,34 +1,34 @@
 const config = {
-  mode: 'site',
-  title: 'JavaScript Guidebook',
-  description: 'JavaScript 完全知识体系',
-  base: '/javascript-guidebook/',
-  publicPath: '/javascript-guidebook/',
-  favicon: './favicon.ico',
-  logo: 'http://img.mrsingsing.com/javascript-guidebook-favicon.png',
-  hash: true,
-  exportStatic: {},
-  navs: [
-    null,
-    {
-      title: 'Github',
-      path: 'https://github.com/tsejx/javascript-guidebook',
-    },
-  ],
-  extraBabelPlugins: [
-    [
-      'import',
-      {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: 'css',
-      },
-    ],
-  ],
+	mode: "site",
+	title: "JavaScript Guidebook",
+	description: "JavaScript 完全知识体系",
+	base: "./",
+	publicPath: "./",
+	favicon: "./favicon.ico",
+	logo: "http://img.mrsingsing.com/javascript-guidebook-favicon.png",
+	hash: true,
+	exportStatic: {},
+	navs: [
+		null,
+		{
+			title: "Github",
+			path: "./javascript-juidebook.png",
+		},
+	],
+	extraBabelPlugins: [
+		[
+			"import",
+			{
+				libraryName: "antd",
+				libraryDirectory: "es",
+				style: "css",
+			},
+		],
+	],
 };
 
-if (process.env.NODE_ENV !== 'development') {
-  config.ssr = {};
+if (process.env.NODE_ENV !== "development") {
+	config.ssr = {};
 }
 
 export default config;
